e1940088dce9080305e10467ffbdf9d1a9470019
Sandrafongshurui <sandrafong90@hotmail.com>
2022-09-02 13:12:10 +0800

    create build

--- src/App.js
+++ src/App.js
@@ -64,10 +64,6 @@ const App = () => {
     list.splice(removeIndx, 1);
     console.log(list);
     setFavouriteList([...list]);
-<<<<<<< HEAD
-=======
-
->>>>>>> 68cc1ff9c5c30f6f75eacc184ce991c5fbbb5b97
   };
 
   const clickShowHideBtn = (value) => {
--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-
-=======
-import reportWebVitals from './reportWebVitals';
->>>>>>> 68cc1ff9c5c30f6f75eacc184ce991c5fbbb5b97
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
